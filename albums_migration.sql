8607d2363f7597f3247cfa7f002128deb3b14dbf
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-25 13:22:23 -0500

    finished part one

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date YEAR (4) NOT NULL,
+    sales INT UNSIGNED NULL,
+    genre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
