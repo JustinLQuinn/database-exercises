3601bee2e60db9953d2b88bbdf61cf8b313a728b
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-25 15:33:14 -0500

    finished albums-seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,9 +2,10 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT ,
     artist VARCHAR(50) NOT NULL,
-    name VARCHAR(100) NOT NULL,
+    album VARCHAR(100) NOT NULL,
     release_date YEAR (4) NOT NULL,
-    sales INT UNSIGNED NULL,
+    sales_in_Millions DECIMAL(3,1) UNSIGNED NULL,
     genre VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
+
---
+++ albums_seeder.sql
@@ -0,0 +1,30 @@
+INSERT INTO albums (artist, album,release_date,sales,genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '50.2', 'Pop, post-disco, funk, rock');
+
+INSERT INTO albums (artist, album,release_date,sales,genre)
+VALUES ('AC/DC', 'Back in Black', '1980', '30', 'Hard rock');
+
+INSERT INTO albums (artist, album,release_date,sales,genre)
+VALUES ('Eagles', 'Their Greatest Hits (1971–1975)', '1976', '41', 'Country rock, soft rock, folk rock');
+
+INSERT INTO albums (artist, album,release_date,sales,genre)
+VALUES ('Eagles', 'Hotel California', '1976', '31', 'Soft rock');
+
+INSERT INTO albums (artist, album,release_date,sales,genre)
+VALUES ('Shania Twain', 'Come On Over', '1997', '30', 'Country, pop');
+
+INSERT INTO albums (artist, album,release_date,sales,genre)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', '1971', '30', 'Hard rock, heavy metal, folk rock');
+
+INSERT INTO albums (artist, album,release_date,sales,genre)
+VALUES  ('Michael Jackson', 'Thriller', '1982', '50', 'Pop, post-disco, funk, rock'),
+        ('AC/DC', 'Back in Black', '1980', '30', 'Hard rock'),
+        ('Eagles', 'Their Greatest Hits (1971–1975)', '1976', '41', 'Country rock, soft rock, folk rock'),
+        ('Eagles', 'Hotel California', '1976', '31', 'Soft rock'),
+        ('Shania Twain', 'Come On Over', '1997', '30', 'Country, pop'),
+        ('Led Zeppelin', 'Led Zeppelin IV', '1971', '30', 'Hard rock, heavy metal, folk rock');
+
+
+UPDATE albums
+SET album = 'Here again'
+WHERE ???? = ???;
